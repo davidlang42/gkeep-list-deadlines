b8922397c847bd6b2427f9251daa0b22fe37c8b1
David Lang <davidlang42@gmail.com>
2024-02-20 00:22:45 +1100

    Basic update and notify

--- notify.py
+++ notify.py
@@ -1,19 +1,27 @@
 import sys
-# import gkeepapi
+import gkeepapi
+import re
 
 if len(sys.argv) != 5:
     print("Expected 4 args: GOOGLE_USERNAME, GOOGLE_TOKEN, GOOGLE_KEEP_LIST_ID, GOOGLE_APPS_SCRIPT_URL")
     exit(1)
 
-# f = open("/gkeep_config/user.txt", "r")
-# g_user = f.read()
-# f.close()
+g_user = sys.argv[1]
+g_token = sys.argv[2]
+list_id = sys.argv[3]
+backend_url = sys.argv[4]
 
-# f = open("/gkeep_config/token.txt", "r")
-# g_token = f.read()
-# f.close()
+keep = gkeepapi.Keep()
+keep.resume(g_user, g_token)
 
-# keep = gkeepapi.Keep()
-# keep.resume(g_user, g_token)
+note = keep.get(list_id)
 
-print("Notify stub")
+items = note.unchecked
+
+pattern = re.compile(r"^\s*\[([^\[\]]+)\]\s*\S+.*$")
+
+for item in items:
+    match = pattern.match(item.text)
+    if match:
+        print(item.text + ": " + match.group(1))
+        #TODO check if any are due, if so trigger due email with list and google keep link
--- update.py
+++ update.py
@@ -1,5 +1,8 @@
 import sys
 import gkeepapi
+import re
+import urllib.request
+import urllib.parse
 
 if len(sys.argv) != 5:
     print("Expected 4 args: GOOGLE_USERNAME, GOOGLE_TOKEN, GOOGLE_KEEP_LIST_ID, GOOGLE_APPS_SCRIPT_URL")
@@ -8,19 +11,35 @@ if len(sys.argv) != 5:
 g_user = sys.argv[1]
 g_token = sys.argv[2]
 list_id = sys.argv[3]
-backend_url = sys.arv[4]
+backend_url = sys.argv[4]
 
 keep = gkeepapi.Keep()
 keep.resume(g_user, g_token)
 
 note = keep.get(list_id)
 
-print(note.title)
-print("^^^^^^^^^^^^^^")
-print(note.text)
+items = note.unchecked
 
-# note = keep.createNote('Todo', 'Eat breakfast')
-# note.pinned = True
-# note.color = gkeepapi.node.ColorValue.Red
+pattern = re.compile(r"^\s*\[([^\[\]]+)\]\s*\S+.*$")
 
-# keep.sync()
+for item in items:
+    match = pattern.match(item.text)
+    if not match:
+        print("Requesting due date for: " + item.text)
+        data = {}
+        data['item'] = item.text
+        data['action'] = 'lookup'
+        data['list_title'] = note.title
+        full_url = backend_url + '?' + urllib.parse.urlencode(data)
+        with urllib.request.urlopen(full_url) as response:
+            bytes = response.read()
+            due = str(bytes, encoding='utf-8')
+            if due:
+                item.text = '[' + due + '] ' + item.text
+                print("Set due date: " + due)
+            else:
+                print("Due date unknown")
+
+note.sort_items() #TODO make sort actually work
+
+keep.sync()
